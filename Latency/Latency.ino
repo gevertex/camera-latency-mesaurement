10e1dcf0b0aefdb63744e194d8a4ad1ab9859b74
George Sapp <george.sapp85@gmail.com>
2017-07-06 21:32:58 -0400

    First working version

--- Latency/Latency.ino
+++ Latency/Latency.ino
@@ -3,12 +3,13 @@ int LED_PIN = 7;
 //ADC pin for photodiode
 int ADC_PIN = 0;
 //Threshold for change required to trigger sample time calculation
-int LIGHT_DELTA_THRESHOLD = 20;
+int LIGHT_DELTA_THRESHOLD = 5;
 //Time measurements above this are considered invalid
-int TIME_OUT_MS = 1000;
+unsigned long TIMEOUT_MS = 1000;
+unsigned long TIMEOUT_US = TIMEOUT_MS * 1000;
 
 //Bias to subtract from current sensor value.  Used to calculate delta
-int light_bias;
+long light_bias;
 
 
 // the setup function runs once when you press reset or power the board
@@ -23,34 +24,63 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  int latencySamples = 10;
-  int currentLatency = 0;
-  unsigned long totalLatencyMs = 0;
+  int latencySamples = 20;
+  long currentLatency = 0;
+  unsigned long totalLatencyUs = 0;
+  unsigned long samplesMeasured = 0;
+  unsigned long minLatency = 100000;
+  unsigned long maxLatency = 0;
 
-  Serial.println("Starting Measurement");
   calibrate();
+  Serial.println("Starting Measurement");
   for (int i=0; i<latencySamples; i++){
-    totalLatencyMs += getCurrLatencyMs();
+    currentLatency = getCurrLatencyUs();
+   
+    Serial.print("Current Latency: ");
+    Serial.print(String(currentLatency/1000));
+    Serial.println("ms");
+
+    if (currentLatency > 0){
+      minLatency = min(currentLatency, minLatency);
+      maxLatency = max(currentLatency, maxLatency);
+      totalLatencyUs += currentLatency;
+      samplesMeasured++;
+    }
   }
   
-  Serial.print("average Latency: ");
-  Serial.println(String(totalLatencyMs/latencySamples));
+  Serial.print("Average Latency: ");
+  if (totalLatencyUs > 0)
+    Serial.print(String(totalLatencyUs/samplesMeasured/1000));
+  else
+    Serial.print(String(totalLatencyUs));
+  Serial.println("ms");
+
+  Serial.print("Max Latency: ");
+  Serial.print(String(maxLatency/1000));
+  Serial.println("ms");
+
+  Serial.print("Min Latency: ");
+  Serial.print(String(minLatency/1000));
+  Serial.println("ms");
+  
+  delay(5000);
 }
 
 //returns -1 if timed out. Returns latency if did not timeout
-int getCurrLatencyMs(){
-  int startTime = 0;
-  int endTime = 0;
-  int elapsedTime = 0;
-  int result;
+long getCurrLatencyUs(){
+  unsigned long startTime = 0;
+  unsigned long endTime = 0;
+  unsigned long elapsedTime = 0;
+  long result;
   
-  startTime = millis();
+  startTime = micros();
   ledOn();
   while (true){
     //Determine if we timed out
-    elapsedTime = millis() - startTime; 
-    if (elapsedTime > TIME_OUT_MS){
-      result = -elapsedTime;
+    elapsedTime = micros() - startTime; 
+    if (elapsedTime > TIMEOUT_US){
+      Serial.println("Wait led on timeout");
+      result = -1;
       break;
     }
 
@@ -61,27 +91,33 @@ int getCurrLatencyMs(){
     }
   }
 
+//  Serial.print("Delta LED On: ");
+//  Serial.println(String(lightDelta()));
+
   ledOff();
 
-  if (result > 0){
-    waitLEDMeasuredOff();
-  }
-  
+  waitLEDMeasuredOff();
+//  Serial.print("Delta LED Off: ");
+//  Serial.println(String(lightDelta()));
+
+
   return result;
 }
 
 void waitLEDMeasuredOff(){
-  int elapsedTime;
-  int startTime = millis();
+  unsigned long elapsedTime;
+  unsigned long startTime = millis();
 
   while(true){
     elapsedTime = millis() - startTime;
     
-    if (elapsedTime > TIME_OUT_MS){
+    if (elapsedTime > TIMEOUT_MS){
+      Serial.println("Wait led off timeout");
       break;
     }
 
-    if (lightDelta() < LIGHT_DELTA_THRESHOLD){
+    if (lightDelta() < 1){
+//      Serial.println("LED measured off");
       break;
     }
     
@@ -109,8 +145,8 @@ void calibrate(){
   Serial.println(analogRead(ADC_PIN));
 }
 
-int averageSamples(int samples){
-  unsigned long total = 0;
+int averageSamples(long samples){
+  long total = 0;
 
   for (int i=0; i<samples; i++){
     total += analogRead(ADC_PIN);
